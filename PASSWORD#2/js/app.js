f03de27813f78e573b33ce3017aef3a4478ddcef
Jean-Luc Noah <74096086+JeanLuc-Noah@users.noreply.github.com>
2021-12-26 12:00:48 +0200

    Patie 2, cacher un mot de passe sur un formulaire

---
+++ PASSWORD#2/js/app.js
@@ -0,0 +1,31 @@
+let inputPassword = document.getElementById("inputPassword")
+let _oei = document.querySelector(".icon-eye");
+
+inputPassword.addEventListener("input",function(){
+    
+    //Si le champs contient quelques
+    if(this.value.length > 0){
+        console.log(this.value);
+        //on ajout la classe visible a l'icon
+        _oei.classList.add('visible')
+
+        //Ecoute l'evenement clique
+        _oei.addEventListener("click", ()=>{
+
+            //Si le type du champ est egale password
+            //On change en text
+            if(this.getAttribute("type") == "password"){
+                this.setAttribute("type", "text") 
+                _oei.innerHTML = '<i class="far fa-eye-slash"></i>';
+            }
+            //Sinon on change text en password
+            else{
+                this.setAttribute("type", "password") 
+                _oei.innerHTML = '<i class="far fa-eye"></i>';
+            }
+        })
+    }
+    else{
+        _oei.classList.remove("visible");
+    }
+})
