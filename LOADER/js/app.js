0fdf66649f52af9e9ddf7010b9a3c9596f4f3c3c
Jean-Luc Noah <74096086+JeanLuc-Noah@users.noreply.github.com>
2021-12-26 11:01:35 +0200

    commit intial

---
+++ LOADER/js/app.js
@@ -0,0 +1,6 @@
+let container_loader = document.querySelector(".container-loader");
+
+//Ecouteur d'evenement
+window.addEventListener("load", ()=>{
+    container_loader.classList.add("hidden");
+})
