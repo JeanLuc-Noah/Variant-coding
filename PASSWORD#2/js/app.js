e05768467f5ef1839607bc8f302ab590dc54f94a
Jean-Luc Noah <74096086+JeanLuc-Noah@users.noreply.github.com>
2021-12-26 12:13:11 +0200

    petite modication au niveau d l'oeil

--- PASSWORD#2/js/app.js
+++ PASSWORD#2/js/app.js
@@ -1,30 +1,31 @@
 let inputPassword = document.getElementById("inputPassword")
 let _oei = document.querySelector(".icon-eye");
 
-inputPassword.addEventListener("input",function(){
-    
+inputPassword.addEventListener("input",function(){ 
     //Si le champs contient quelques
     if(this.value.length > 0){
+
         //on ajout la classe visible a l'icon
         _oei.classList.add('visible')
-
-        //Ecoute l'evenement clique
-        _oei.addEventListener("click", ()=>{
-
-            //Si le type du champ est egale password
-            //On change en text
-            if(this.getAttribute("type") == "password"){
-                this.setAttribute("type", "text") 
-                _oei.innerHTML = '<i class="far fa-eye-slash"></i>';
-            }
-            //Sinon on change text en password
-            else{
-                this.setAttribute("type", "password") 
-                _oei.innerHTML = '<i class="far fa-eye"></i>';
-            }
-        })
     }
     else{
         _oei.classList.remove("visible");
+    }
+})
+
+//Ecoute l'evenement clique
+_oei.addEventListener("click", function (){
+
+    //Si l'attribut de l'input est egale password
+    //On le change text
+    if(inputPassword.getAttribute("type") == "password"){
+
+        inputPassword.setAttribute("type", "text")
+        _oei.innerHTML = '<i class="far fa-eye-slash"></i>';
+    }
+    //Sinon le change en password
+    else{
+        inputPassword.setAttribute("type", "password")
+        _oei.innerHTML = '<i class="far fa-eye"></i>';
     }
 })
